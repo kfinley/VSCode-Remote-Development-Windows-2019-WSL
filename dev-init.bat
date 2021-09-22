05e702a3e09cb4d1cde7f4f234fc31cf3c6aaf8e
Kyle Finley <kyle@swingu.com>
2021-09-22 15:50:07 -0700

    .NET: Added 4.7.2 install and cleaned up dev-init.bat

--- dev-init.bat
+++ dev-init.bat
@@ -2,11 +2,20 @@ echo
 echo 'Running dev initialization script...'
 echo
 
-echo 'Installing .NET Framework 4.8 SDK'
+set DOTNET_FRAMEWORK_VERSION=%1
+set "DOTNET_URL="
 
-wget https://download.visualstudio.microsoft.com/download/pr/2d6bb6b2-226a-4baa-bdec-798822606ff1/8494001c276a4b96804cde7829c04d7f/ndp48-x86-x64-allos-enu.exe
-chmod +x ndp48-x86-x64-allos-enu.exe
-./ndp48-x86-x64-allos-enu.exe /q /norestart /ChainingPackage
+echo 'Installing .NET Framework %DOTNET_FRAMEWORK_VERSION% SDK'
+
+IF [%DOTNET_FRAMEWORK_VERSION%] == "4.7" %DOTNET_URL%=https://download.visualstudio.microsoft.com/download/pr/1f5af042-d0e4-4002-9c59-9ba66bcf15f6/089f837de42708daacaae7c04b7494db/ndp472-kb4054530-x86-x64-allos-enu.exe
+IF [%DOTNET_FRAMEWORK_VERSION%] == "4.8" %DOTNET_URL%=https://download.visualstudio.microsoft.com/download/pr/2d6bb6b2-226a-4baa-bdec-798822606ff1/8494001c276a4b96804cde7829c04d7f/ndp48-x86-x64-allos-enu.exe
+
+wget %DOTNET_URL%
+
+@REM chmod +x ndp48-x86-x64-allos-enu.exe
+
+IF [%DOTNET_FRAMEWORK_VERSION%] == "4.7" ./ndp472-kb4054530-x86-x64-allos-enu.exe /q /norestart /ChainingPackage
+IF [%DOTNET_FRAMEWORK_VERSION%] == "4.8" ./ndp48-x86-x64-allos-enu.exe /q /norestart /ChainingPackage
 
 @REM echo 'Installing .NET Framework 4.8 SDK'
 
@@ -16,19 +25,21 @@ chmod +x ndp48-x86-x64-allos-enu.exe
 @REM Download the Build Tools bootstrapper.
 curl -SL --output vs_buildtools.exe https://aka.ms/vs/16/release/vs_buildtools.exe
 
-chmod +x vs_buildtools.exe
+@REM chmod +x vs_buildtools.exe
 
 @REM Run this section from Windows cmd 
 vs_buildtools.exe install --wait --quiet --norestart --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools" --nocache  --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Workload.WebBuildTools 
 
-curl -SL --output ndp48-devpack-enu.exe https://download.visualstudio.microsoft.com/download/pr/714a99a2-db28-432e-9a39-4345ba11e73f/5108686aec021898cec3de2cc4d9fd3c/ndp48-devpack-enu.exe
+set "DOTNET_DEVPACK="
+IF [%DOTNET_FRAMEWORK_VERSION%] == "4.7" %DOTNET_DEVPACK%=https://download.visualstudio.microsoft.com/download/pr/158dce74-251c-4af3-b8cc-4608621341c8/9c1e178a11f55478e2112714a3897c1a/ndp472-devpack-enu.exe
+IF [%DOTNET_FRAMEWORK_VERSION%] == "4.8" %DOTNET_DEVPACK%=https://download.visualstudio.microsoft.com/download/pr/714a99a2-db28-432e-9a39-4345ba11e73f/5108686aec021898cec3de2cc4d9fd3c/ndp48-devpack-enu.exe
 
-ndp48-devpack-enu.exe /install /quiet /norestart
+wget %DOTNET_DEVPACK%
 
-curl -SL --output nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+IF [%DOTNET_FRAMEWORK_VERSION%] == "4.7" ndp472-devpack-enu.exe /install /quiet /norestart
+IF [%DOTNET_FRAMEWORK_VERSION%] == "4.8" ndp48-devpack-enu.exe /install /quiet /norestart
+
+wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 
 mkdir -p "%ProgramFiles(x86)%/nuget/"
 mv nuget.exe "%ProgramFiles(x86)%/nuget/"
-
-@REM Cleanup
-del vs_buildtools.exe
